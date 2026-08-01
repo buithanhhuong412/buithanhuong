15a3e681b11ff0ea49eb9888913313ab25c574c6
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-08-01 15:22:18 +0700

    Update popup-7.ts

--- data/experiment/popup-7.ts
+++ data/experiment/popup-7.ts
@@ -2,8 +2,8 @@ import type { PageExperimentPopup } from './types';
 import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: PageExperimentPopup = {
-    slug: 'kickstart',
-    title: 'kickstart',
+    slug: 'kickstar',
+    title: 'kickstar',
     img: 'images/8.png',
     scale: 0.1,
     marginRight: 50,
