7b86548f803273d4fd9eb29dab8372613e90a6c6
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-08-02 20:26:04 +0700

    Update popup-2.ts

--- data/observe/popup-2.ts
+++ data/observe/popup-2.ts
@@ -3,7 +3,7 @@ import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: ObservePopup = {
     slug: 'digital-product-vs-architecture',
-    text: "Digital Product vs Architecture - a type of huge physical product",
+    text: "Digital Product vs Architecture - a large-scale physical product",
     image: {
         src: 'images/study_3.png',
         width: '160px',
