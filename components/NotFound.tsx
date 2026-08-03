26ab6d1955d73b1453513862911aabacab90b1f7
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-08-03 20:58:56 +0700

    Update NotFound.tsx

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -25,7 +25,7 @@ export default function NotFound() {
         </p>
 
          <img
-          src="/images/pat2.png"
+          src="/images/Frame 7.svg"
           alt="404 illustration"
           className="mx-auto mt-32 w-48"
         />
