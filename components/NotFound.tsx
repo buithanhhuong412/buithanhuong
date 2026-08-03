4967de733fd2590613e5ae636aed35324be3a9a2
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-08-03 22:58:17 +0700

    Update NotFound.tsx

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -23,7 +23,7 @@ export default function NotFound() {
             :))
         </p>
 
-         <div className="relative mx-auto mt-12 w-[230px] h-[190px] dog cursor-pointer">
+         <div className="relative mx-auto mt-12 w-[230px] h-[190px] dog cursor-grab">
             <img
                 src="/images/coca_body.svg"
                 alt=""
