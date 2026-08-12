a4cea63759005ff61b8ad4c12b90d03fbe324137
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-08-12 09:27:00 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentPage }) => {
   const menuItems = [
     { id: 'about', label: 'I', path: '/about' },
     { id: 'study', label: 'study', path: '/study' },
-    { id: 'observe', label: 'observe', path: '/observe' },
+    { id: 'observe', label: 'observe', path: 'https://www.threads.com/@buithanhuong_as_usual', external: true },
     { id: 'work', label: 'and work', path: '/experiment' }
   ];
 
