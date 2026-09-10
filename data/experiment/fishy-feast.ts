b698b7ed86f8242485c09f5b26a76fd50caa8f66
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-10 17:21:11 +0700

    Update fishy-feast.ts

--- data/experiment/fishy-feast.ts
+++ data/experiment/fishy-feast.ts
@@ -3,7 +3,7 @@ import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: PageExperimentPopup = {
     slug: 'fishy-feast',
-    title: 'fishy feast',
+    title: 'Fishy Feast - Blockchain game',
     img: '/images/5.avif',
     scale: 0.05,
     marginRight: 30,
