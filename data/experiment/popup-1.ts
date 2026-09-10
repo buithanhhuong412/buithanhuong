28199a25c0f95c1937890839734fb974a38017c7
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-10 15:04:00 +0700

    Update popup-1.ts

--- data/experiment/popup-1.ts
+++ data/experiment/popup-1.ts
@@ -3,7 +3,8 @@ import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: PageExperimentPopup = {
     slug: 'vgbc-green-building-platform',
-    title: 'VGBC \n Green Building Platform',
+    title: 'VGBC
+    Green Building Platform',
     img: '/images/2.avif',
     scale: 0.1,
     marginRight: 85,
