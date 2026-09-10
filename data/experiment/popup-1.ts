476454f22ce05179fcf60fdad3ca053b2a4413bf
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-10 15:06:23 +0700

    Update popup-1.ts

--- data/experiment/popup-1.ts
+++ data/experiment/popup-1.ts
@@ -4,7 +4,7 @@ import { DEFAULT_ARTICLE } from '../thought-content';
 const popup: PageExperimentPopup = {
     slug: 'vgbc-green-building-platform',
     title: 'VGBC
-    Green Building Platform',
+    Vietnam Green Building Platform',
     img: '/images/2.avif',
     scale: 0.1,
     marginRight: 85,
