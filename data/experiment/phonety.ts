bd6d0cc7f23ac2512484b524c6df1ec735001c4e
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-19 21:37:10 +0700

    Create phonety.ts

---
+++ data/experiment/phonety.ts
@@ -0,0 +1,13 @@
+import type { PageExperimentPopup } from './types';
+import { DEFAULT_ARTICLE } from '../thought-content';
+
+const popup: PageExperimentPopup = {
+    slug: 'phonety',
+    title: 'Phonety',
+    img: '/images/1.avif',
+    scale: 0.1,
+    marginRight: 110,
+    article: DEFAULT_ARTICLE,
+};
+
+export default popup;
