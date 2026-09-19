d495a9b6f66d5b189a5f452821c51b040c913ea1
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-19 21:39:52 +0700

    Update theu-mua-thu.ts

--- data/experiment/theu-mua-thu.ts
+++ data/experiment/theu-mua-thu.ts
@@ -2,9 +2,9 @@ import type { PageExperimentPopup } from './types';
 import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: PageExperimentPopup = {
-    slug: 'phonety',
-    title: 'Phonety - English IPA Pronunciation',
-    img: '/images/1.avif',
+    slug: 'theu-mua-thu',
+    title: 'Thêu một mùa thu',
+    img: '/images/123.avif',
     scale: 0.1,
     marginRight: 110,
     article: DEFAULT_ARTICLE,
