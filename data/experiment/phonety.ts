90f01a88673a145b1b9cbe45152a26cdab4bd5d7
buithanhhuong412 <buithanhhuong412@gmail.com>
2026-09-23 22:04:50 +0700

    Update phonety.ts

--- data/experiment/phonety.ts
+++ data/experiment/phonety.ts
@@ -1,13 +1,30 @@
 import type { PageExperimentPopup } from './types';
-import { DEFAULT_ARTICLE } from '../thought-content';
 
 const popup: PageExperimentPopup = {
-    slug: 'phonety',
-    title: 'Phonety - English IPA Pronunciation',
-    img: '/images/phonety_avatar.png',
-    scale: 0.075,
-    marginRight: 110,
-    article: DEFAULT_ARTICLE,
+  slug: 'phonety',
+
+  title: 'Phonety - English IPA Pronunciation',
+
+  img: '/images/phonety_avatar.png',
+
+  scale: 0.075,
+
+  marginRight: 110,
+
+  article: {
+    intro: '',
+    blocks: [
+      {
+        type: 'grid',
+        items: [
+          {
+            title: '',
+            desc: 'Phonety is an English IPA pronunciation project. Visit http://phonety.vercel.app to explore the project.',
+          },
+        ],
+      },
+    ],
+  },
 };
 
 export default popup;
